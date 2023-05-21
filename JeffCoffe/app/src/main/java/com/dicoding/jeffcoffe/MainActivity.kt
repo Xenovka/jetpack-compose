1a7d134779af3c7d49cf1f8e9401c6ea220fba77
Xenovka <wendyanto18@gmail.com>
2023-05-21 18:45:18 +0700

    creating menu and implementing slot-based layout

--- JeffCoffe/app/src/main/java/com/dicoding/jeffcoffe/MainActivity.kt
+++ JeffCoffe/app/src/main/java/com/dicoding/jeffcoffe/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,10 +20,11 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.dicoding.jeffcoffe.model.Menu
+import com.dicoding.jeffcoffe.model.dummyBestSellerMenu
 import com.dicoding.jeffcoffe.model.dummyCategory
-import com.dicoding.jeffcoffe.ui.components.CategoryItem
-import com.dicoding.jeffcoffe.ui.components.SearchBar
-import com.dicoding.jeffcoffe.ui.components.SectionText
+import com.dicoding.jeffcoffe.model.dummyMenu
+import com.dicoding.jeffcoffe.ui.components.*
 import com.dicoding.jeffcoffe.ui.theme.JeffCoffeTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,11 +39,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun JeffCoffeeApp(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
+fun JeffCoffeeApp() {
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
         Banner()
-        SectionText(stringResource(R.string.section_category))
-        CategoryRow()
+        HomeSection(
+            title = stringResource(R.string.section_category),
+            content = { CategoryRow() }
+        )
+        HomeSection(
+            title = stringResource(R.string.section_favorite_menu),
+            modifier = Modifier,
+            content = { MenuRow(dummyMenu) }
+        )
+        HomeSection(
+            title = stringResource(R.string.section_best_seller_menu),
+            content = { MenuRow(dummyBestSellerMenu) }
+        )
     }
 }
 
@@ -72,13 +86,29 @@ fun CategoryRow(
     }
 }
 
-//@Composable
-//@Preview(showBackground = true)
-//fun CategoryRowPreview() {
-//    JeffCoffeTheme {
-//        CategoryRow()
-//    }
-//}
+@Composable
+fun MenuRow(
+    listMenu: List<Menu>,
+    modifier: Modifier = Modifier
+)  {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(listMenu, key = { it.title }) { menu ->
+            MenuItem(menu)
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun CategoryRowPreview() {
+    JeffCoffeTheme {
+        CategoryRow()
+    }
+}
 
 @Preview(showBackground = true, device = Devices.PIXEL_4)
 @Composable
