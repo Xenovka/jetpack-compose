4aa231588f916c256fd25c29dd855b6eb5740d6e
Xenovka <wendyanto18@gmail.com>
2023-05-21 18:06:29 +0700

    creating a banner

--- JeffCoffe/app/src/main/java/com/dicoding/jeffcoffe/MainActivity.kt
+++ JeffCoffe/app/src/main/java/com/dicoding/jeffcoffe/MainActivity.kt
@@ -3,13 +3,22 @@ package com.dicoding.jeffcoffe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dicoding.jeffcoffe.ui.components.SearchBar
 import com.dicoding.jeffcoffe.ui.theme.JeffCoffeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +26,36 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JeffCoffeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                JeffCoffeeApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun JeffCoffeeApp() {
+    Column() {
+        Banner()
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun Banner(modifier: Modifier = Modifier) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
+        SearchBar()
+    }
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_4)
+@Composable
+fun JeffCoffeeAppPreview() {
     JeffCoffeTheme {
-        Greeting("Android")
+        JeffCoffeeApp()
     }
 }
