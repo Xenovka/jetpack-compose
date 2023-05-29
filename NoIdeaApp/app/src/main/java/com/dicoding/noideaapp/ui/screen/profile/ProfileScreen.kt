9dd43242ddfefd1d958c2c63332c36734744ae35
Xenovka <wendyanto18@gmail.com>
2023-05-29 18:41:28 +0700

    about page

--- NoIdeaApp/app/src/main/java/com/dicoding/noideaapp/ui/screen/profile/ProfileScreen.kt
+++ NoIdeaApp/app/src/main/java/com/dicoding/noideaapp/ui/screen/profile/ProfileScreen.kt
@@ -1,22 +1,69 @@
 package com.dicoding.noideaapp.ui.screen.profile
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.dicoding.noideaapp.R
+import com.dicoding.noideaapp.ui.theme.NoIdeaAppTheme
 
 @Composable
 fun ProfileScreen(
     modifier: Modifier = Modifier
 ) {
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(40.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(stringResource(R.string.menu_profile))
+        Image(
+            painter = painterResource(R.drawable.profile),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = modifier
+                .clip(CircleShape)
+                .size(300.dp)
+        )
+        Text(
+            text = stringResource(R.string.creatorName),
+            style = MaterialTheme.typography.headlineMedium,
+            fontWeight = FontWeight.Medium,
+            modifier = modifier.padding(top = 22.dp)
+        )
+        Text(
+            text = stringResource(R.string.creatorEmail),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = modifier.padding(top = 12.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProfileScreenPreview() {
+    NoIdeaAppTheme {
+        ProfileScreen()
     }
 }
